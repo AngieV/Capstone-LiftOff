5a7597be87915f95ad405bc001ea2a60a99e8d4b
AngieV <angiev@me.com>
2021-09-12 12:12:34 -0500

    add controllers and models

---
+++ src/main/java/org/launchcode/Songs4Soldiers/Songs4SoldiersApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Songs4Soldiers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Songs4SoldiersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Songs4SoldiersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Songs4Soldiers/Songs4SoldiersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Songs4Soldiers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Songs4SoldiersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
