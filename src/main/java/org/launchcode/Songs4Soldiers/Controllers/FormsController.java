34ecc072d3e7b0210fcf2e29dcfebd778e788b87
AngieV <angiev@me.com>
2021-09-15 00:15:40 -0500

    enable Validation on Veteran Assist Form

--- src/main/java/org/launchcode/Songs4Soldiers/Controllers/FormsController.java
+++ src/main/java/org/launchcode/Songs4Soldiers/Controllers/FormsController.java
@@ -13,7 +13,9 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -24,11 +26,11 @@ public class FormsController {
     private static List<Volunteer> volunteer = new ArrayList<>();
 
     @GetMapping("/contact")
-    public String displayVetAssistForm(Model model) {
+    public String displayVetAssistanceForm(Model model) {
         model.addAttribute("title_vet", "Apply for Assistance");
         model.addAttribute(new Veteran());
-        //Veteran veteran = new Veteran();
-        //model.addAttribute("veteran", veteran);
+        List<String> branchList = Arrays.asList("US Army", "US Navy", "US Air Force", "US Marines", "US Coast Guard");
+        model.addAttribute("branchList", branchList);
         return "S4S/contact";
     }
 
@@ -37,30 +39,29 @@ public class FormsController {
                                           Errors errors, Model model) {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Contact Us");
-            //model.addAttribute("errorMsg", "Bad data!");
             return "S4S/contact";
         }
-        System.out.println(veteran);
+        //System.out.println(veteran);
+        UserData.add(veteran);
         return "redirect:registered";
     }
-/*
-    @GetMapping("/contact")
-    public String displayVolunteerForm(Model model) {
+
+    @GetMapping("contact/vol")
+    public String displayVolunteerForm(Model model){
         model.addAttribute("title_vol", "Volunteer");
-        Volunteer volunteer = new Volunteer();
-        model.addAttribute("volunteer", volunteer);
+        model.addAttribute(new Volunteer());
+        //model.addAttribute("volunteer", volunteer);
         return "S4S/contact";
     }
 
-    @PostMapping("/contact")
-    public String createVolunteer(@ModelAttribute("volunteer") Volunteer volunteer){
-        System.out.println(volunteer);
+    @PostMapping("/contact/vol")
+    public String createVolunteer(@ModelAttribute("volunteer") Volunteer volunteer, Model model){
+        //System.out.println(volunteer);
+        UserData.add(volunteer);
         return "redirect:registered";
     }
 
-    /*
-
-    @GetMapping("/purchases")
+    /*     @GetMapping("/purchases")
     public String displayPurchases(Model model){
         return "S4S/purchases";
     }
@@ -70,7 +71,6 @@ public class FormsController {
                                   Errors errors, Model model) {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Contact Us");
-            model.addAttribute("errorMsg", "Bad data!");
             return "S4S/contact";
         }
         UserData.add(volunteer);
--- src/main/java/org/launchcode/Songs4Soldiers/Controllers/S4SController.java
+++ src/main/java/org/launchcode/Songs4Soldiers/Controllers/S4SController.java
@@ -4,17 +4,9 @@ package org.launchcode.Songs4Soldiers.Controllers;
 ©Angela Volluz 2021
 */
 
-import org.launchcode.Songs4Soldiers.models.Veteran;
-import org.launchcode.Songs4Soldiers.models.Volunteer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping(value="S4S")
@@ -41,6 +33,7 @@ public class S4SController {
     @RequestMapping(value="shop")
     public String shop(Model model){
         model.addAttribute("title", "Merchandise");
+        model.addAttribute("tickets", "Tickets");
         return "S4S/shop";
     }
 
@@ -51,7 +44,7 @@ public class S4SController {
     }
 
     @RequestMapping(value="registered")
-    public String showRegistration( Model model) {
+    public String showRegistration(Model model) {
         model.addAttribute("title", "Registration");
         return "S4S/registered";
     }
@@ -61,5 +54,4 @@ public class S4SController {
         model.addAttribute("title", "Your Cart");
         return "S4S/purchases";
     }
-
 }
